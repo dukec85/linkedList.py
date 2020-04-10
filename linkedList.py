46ca243b59bb26f36edf2a739c207f387caad74b
Christopher Duke <cd.duke@gmail.com>
2020-04-10 11:46:35 -0500

    added file

---
+++ linkedList.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+
+    def get_next_node(self):
+        return self.next_node
+
+    def get_value(self):
+        return self.value
+
+class LinkedList:
+    def __init__(self, value=None):
+        self.head_node = Node(value)
+
+    def get_head_node(self):
+        return self.head_node
+
+    def insert_start(self, new_value):
+        new_node = Node(new_value)
+        new_node.set_next_node(self.head_node)
+        self.head_node = new_node
+
+    def list_value(self):
+        current_node = self.head_node
+        while current_node:
+            if current_node.get_value() != None:
+                print(current_node.get_value())
+                current_node = current_node.get_next_node()
+
+    def remove(self, value_to_remove):
+        current_node = self.head_node
+        if current_node.get_value() == value_to_remove:
+            self.head_node = current_node.get_next_node()
+        else:
+            while current_node:
+                next_node = current_node.get_next_node()
+                if next_node.get_value() == value_to_remove:
+                    current_node.set_next_node(next_node.get_next_node())
+                    current_node = None
+                else:
+                    current_node = next_node
+
+ll = LinkedList(5)
+ll.insert_start(77)
+ll.insert_start(11473)
+ll.insert_start(90)
+print(ll.list_value())
